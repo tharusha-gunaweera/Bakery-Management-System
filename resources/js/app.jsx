4afa073427463d179c6c48718cc466e5f2f24bc7
tharusha-gunaweera <tharushagunaweera@gmail.com>
2025-08-22 03:15:49 +0530

    Initial commit: Add Bakery-Management-System project

---
+++ resources/js/app.jsx
@@ -0,0 +1,20 @@
+
+import '../css/app.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+    
+});
