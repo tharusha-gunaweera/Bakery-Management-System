a806758b734402379147191546a510dfeb63efdb
Kalpani1125 <sahana200225@gmail.com>
2025-09-02 01:51:42 -0700

    backend folder structure creater

---
+++ backend/Controlers/UserControllers.js
@@ -0,0 +1,111 @@
+const User = require("../Model/UserModel");
+
+//data display
+
+const getAllUsers = async (req, res, next)=> {
+    let Users;
+
+    //get all users
+    try{
+        users = await User.find();
+    }catch (err) {
+        console.log(err);
+    }
+
+    //not found
+    if(!users){
+        return res.status(404).json({message: "User not found"});
+    }
+    //display all users
+    return res.status(200).json({users});
+};
+
+const addUsers = async (req, res, next) => {
+    const{name, email, password} = req.body;
+    let users;
+
+    try{
+        const newUser = new User(req.body); 
+        await newUser.save();
+        res.status(200).json(newUser);
+    }catch (err){
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+    
+    if(!users){
+        return res.status(404).send({message:"unable to add users"});
+    }
+    return res.status(404).json({users});
+
+};
+
+const getById = async(req,res, next)=>{
+    const id = req.params.id;
+
+    let users;
+
+    try{
+        users = await User.findById(id);
+    }catch (err){
+        console.log(err);
+    }
+    
+    if(!users){
+        return res.status(404).send({message:"unable to add users"});
+    }
+    return res.status(200).json({users});
+
+};
+
+
+const updateUser = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, email, password} = req.body;
+
+    let users;
+
+    try {
+        users = await User.findByIdAndUpdate(id,
+            { name: name, email: email, password: password });
+            users = await users.save();
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!users) {
+        return res.status(404).json({ message: "Unable to update user" });
+    }
+
+    return res.status(200).json({users});
+};
+
+
+
+const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+    let user;
+
+    try {
+        users = await User.findByIdAndDelete(id);
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!users) {
+        return res.status(404).json({ message: "Unable to delete user" });
+    }
+
+    return res.status(200).json({users});
+};
+
+
+
+
+exports.getAllUsers = getAllUsers;
+exports.addUsers = addUsers;
+exports.getById = getById; 
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
+
+
--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,20 @@
+// mongodb username - sahana2002 & mongodb password - QhO31A3P9hnHysvq
 
-console.log("Backend server is running...");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./Routes/UserRoutes");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use("/users",router);
+
+
+mongoose.connect("mongodb+srv://sahana2002:QhO31A3P9hnHysvq@cluster0.wptflp0.mongodb.net/")
+.then(()=> console.log("connected to mongoDB"))
+.then(()=> {
+    app.listen(4000);
+})
+.catch((err)=> console.log((err))); 
+
